85105e12f0ab098088316e67e277817641d8a7e3
Mohamed <mohamed.safwat@systemira.net>
2025-04-11 23:25:30 +0200

    Initial Commit

---
+++ MazaadyIOsTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MazaadyIOsTask
+//
+//  Created by Moha on 4/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MazaadyIOsTaskTests/MazaadyIOsTaskTests.swift
@@ -0,0 +1,17 @@
+//
+//  MazaadyIOsTaskTests.swift
+//  MazaadyIOsTaskTests
+//
+//  Created by Moha on 4/11/25.
+//
+
+import Testing
+@testable import MazaadyIOsTask
+
+struct MazaadyIOsTaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
